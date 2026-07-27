adf64fd04235bf197c00736d69841b20915dff28
asadullah <asadulllahbilal@gmail.com>
2026-07-27 14:04:20 +0500

    output parsers completed

---
+++ Langchain/Langchain_output_parsers/structuredoutputparser.py
@@ -0,0 +1,35 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv  
+from langchain_core.prompts import PromptTemplate
+from langchain_classic.output_parsers.structured import StructuredOutputParser, ResponseSchema
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Pro",
+    task="text-generation"
+)
+
+model = ChatHuggingFace(llm=llm)
+
+schema = [
+    ResponseSchema(name='fact_1', description='Fact 1 about the topic'),
+    ResponseSchema(name='fact_2', description='Fact 2 about the topic'),
+    ResponseSchema(name='fact_3', description='Fact 3 about the topic'),
+    ResponseSchema(name='fact_4', description='Fact 4 about the topic'),
+    ResponseSchema(name='fact_5', description='Fact 5 about the topic'),
+
+]
+
+parser = StructuredOutputParser.from_response_schemas(schema)
+
+template = PromptTemplate(
+    template='Give 5 fact about {topic} \n {format_instruction}',
+    input_variables=['topic'],
+    partial_variables={'format_instruction':parser.get_format_instructions()}
+)
+
+chain = template | model | parser
+
+result = chain.invoke({'topic':'black hole'})
+
+print(result)
